992781d76b771f946bf2c46971f1da8df9d035d2
Jessica Fernandez <jferasef@gmail.com>
2022-04-06 22:25:00 +0200

    add testing method

--- js/index.js
+++ js/index.js
@@ -12,22 +12,58 @@ class Game {
     this.spawnInterval = 3 * this.fps;
     this.avatar = new Avatar(this.canvas, this.ctx);
     this.ingr = new Ingredients(this.canvas, this.ctx, this.moveSpeed);
+    this.floor = {
+      x: 0,
+      y: this.canvas.height,
+      width: Infinity,
+      height: Infinity,
+    };
+    this.lengthBreadGoal = 3;
     this.timer = 0;
+  }
+
+  init() {
+    this.breadGoal = this.ingr.createGoalBread(this.lengthBreadGoal);
+    this.breadCurrent = [];
   }
 
   start() {
     console.log("start");
+    this.init();
     this.createEventListeners();
     setInterval(() => this.update(), 1000 / this.fps);
   }
 
   update() {
     console.log("update");
+    if (this.lengthBreadGoal === 5) {
+      console.debug("Win");
+      alert("win");
+    }
+    if (this.breadCurrent.length === this.breadGoal.length) {
+      this.lengthBreadGoal++;
+      this.init();
+    }
     this.timer++;
     this.clear();
     this.avatar.draw();
     this.ingr.draw();
     this.ingr.move();
+    const nextIngr = [];
+    for (const ingred of this.ingr.ingredients) {
+      if (Ingredients.intersects(this.avatar, ingred)) {
+        if (this.breadGoal[this.breadCurrent.length].name === ingred.name) {
+          this.breadCurrent.push(ingred);
+        } else {
+          console.log("loose");
+          alert("loose");
+          break;
+        }
+      } else if (!Ingredients.intersects(this.floor, ingred)) {
+        nextIngr.push(ingred);
+      }
+    }
+    this.ingr.ingredients = nextIngr;
     if (this.timer % this.spawnInterval === 0) {
       this.ingr.addRandomIngredient();
     }
@@ -40,23 +76,4 @@ class Game {
   createEventListeners() {
     document.addEventListener("keydown", (e) => this.avatar.move(e.key));
   }
-}
-
-function pickIngredients() {
-  let pickedIng = [];
-  let picked = false;
-  for (let i = 0; i < ingredients.length; i++) {
-    const ing = ingredients[i];
-    const withinX =
-      avatar.x + avatar.width > ingr.x && avatar.x < ingr.x + ingr.width;
-    const withinY =
-      ingr.y + ingr.height > avatar.y && ingr.y < ingr.y + ingr.height;
-
-    picked = withinX && withinY;
-
-    if (picked) {
-      break;
-    }
-  }
-  return picked;
 }
--- js/ingredients.js
+++ js/ingredients.js
@@ -7,14 +7,39 @@ function getImage(name) {
 }
 
 class Ingredients {
-  ingredientsModel = [
-    { type: "image", name: "tomatoe", img: getImage("tomatoe.png") },
-    { type: "image", name: "cheese", img: getImage("cheese.png") },
-    { type: "image", name: "bread", img: getImage("bread.png") },
+  ingredientsInsideBreadModel = [
+    {
+      type: "image",
+      name: "tomatoe",
+      img: getImage("tomatoe.png"),
+      width: 30,
+      height: 30,
+    },
+    {
+      type: "image",
+      name: "cheese",
+      img: getImage("cheese.png"),
+      width: 30,
+      height: 30,
+    },
+    {
+      type: "image",
+      name: "burguer",
+      img: getImage("meat.png"),
+      width: 30,
+      height: 30,
+    },
   ];
-
-  width = 30;
-  height = 30;
+  ingredientsModel = [
+    ...this.ingredientsInsideBreadModel,
+    {
+      type: "image",
+      name: "bread",
+      img: getImage("bread.png"),
+      width: 30,
+      height: 30,
+    },
+  ];
 
   constructor(canvas, ctx, moveSpeed) {
     this.canvas = canvas;
@@ -24,6 +49,19 @@ class Ingredients {
     this.maxX = canvas.width - this.width;
     this.maxY = canvas.height - this.height;
     this.addRandomIngredient();
+  }
+
+  createGoalBread(amountOfIng) {
+    const insideIng = [];
+    insideIng.push(this.ingredientsModel[this.ingredientsModel.length - 1]);
+    for (let i = 0; i < amountOfIng; i++) {
+      const idx = Math.floor(
+        Math.random() * this.ingredientsInsideBreadModel.length
+      );
+      insideIng.push(this.ingredientsModel[idx]);
+    }
+    insideIng.push(this.ingredientsModel[this.ingredientsModel.length - 1]);
+    return insideIng;
   }
 
   addRandomIngredient() {
@@ -43,7 +81,7 @@ class Ingredients {
       ...ing,
       y: ing.y + this.moveSpeed,
     }));
-    // this.ingredients = this.ingredients.map(({y, ...props}) => ({...props, y: y + 1}));
+    //this.ingredients = this.ingredients.map(({y, ...props}) => ({...props, y: y + 1}));
   }
 
   draw() {
@@ -51,15 +89,26 @@ class Ingredients {
     this.ingredients.forEach((ing) => {
       switch (ing.type) {
         case "image":
-          console.log(ing);
+          // console.log(ing);
           this.ctx.drawImage(ing.img, ing.x, ing.y, this.width, this.height);
           break;
-
         default:
           break;
       }
     });
   }
 
-  // ingredients will only move along y
+  static intersects(obj, ingr) {
+    const minX = obj.x;
+    const minY = obj.y;
+    const maxX = obj.x + obj.width;
+    const maxY = obj.y + obj.height;
+    const withinX = minX <= ingr.x && ingr.x + ingr.width <= maxX;
+    const withinY = minY <= ingr.y && ingr.y + ingr.height <= maxY;
+    console.log({ minX, minY, maxX, maxY, withinX, withinY, ingr });
+    if (withinX && withinY) {
+      console.debug("intersect");
+    }
+    return withinX && withinY;
+  }
 }
